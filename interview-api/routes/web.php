c243a4720eef2b123cd93e543c370213a3df3e4d
Lukasz <woryszlukasz@gmail.com>
2025-01-19 17:30:36 +0100

    Crud

--- interview-api/routes/web.php
+++ interview-api/routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# Creating
+Route::get('/pets/create', [PetController::class, 'create']);
+
+# Adding
+Route::post('/pets', [PetController::class, 'store']);
+
+# Getting
+Route::get('/pets', [PetController::class, 'index']);
+
+# Edditing
+Route::get('/pets/{id}/edit', [PetController::class, 'edit']);
+
+# Updating
+Route::patch('/pets/{id}', [PetController::class, 'update']);
+
+# Deleting
+Route::delete('/pets/{id}', [PetController::class, 'destroy']);
